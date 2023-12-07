91c9b008736c519dadffbfb23e79b0388dcac253
Wizerr <127906404+Wizerr@users.noreply.github.com>
2023-12-07 20:28:24 +0100

    Update BareMetal.java

--- Mathias/src/model/BareMetal.java
+++ Mathias/src/model/BareMetal.java
@@ -1,5 +1,33 @@
 package model;
 
+import java.util.HashSet;
+
 public class BareMetal extends Server {
+	
+    private HashSet<Server> hostedServers;
+
+    public BareMetal() {
+        hostedServers = new HashSet<>();
+    }
+
+    public void addHostedServer(Server server) {
+        if (!(server instanceof BareMetal)) {	
+        	//les serveurs BareMetal ne peuvent pas être hébergés
+            hostedServers.add(server);
+        } else {
+            throw new IllegalArgumentException("Un serveur BareMetal ne peut pas être hébergé.");
+        }
+    }
+    
+    @Override
+    public void setState(State state) {
+        super.setState(state);
+        if (state == State.DOWN) {
+            // Si le serveur BareMetal est éteint, éteindre tous les serveurs hébergés
+            for (Server hostedServer : hostedServers) {
+                hostedServer.setState(State.DOWN);
+            }
+        }
+    }
 
 }
