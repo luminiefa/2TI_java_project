8b90041bbd8df17cd08bfb8f4154e897a24aef43
Wizerr <127906404+Wizerr@users.noreply.github.com>
2023-12-07 22:23:43 +0100

    Update Container.java

--- Mathias/src/model/Container.java
+++ Mathias/src/model/Container.java
@@ -2,6 +2,22 @@ package model;
 
 public class Container extends Server {
 	
-    private Service installedService; //Un seul service
+    public Container() {};
 
+    
+    @Override
+    public void installService(Service service) {
+        // Vérifier qu'il n'y a pas encore de service installé car un Container ne peut avoir qu'un service
+    	if (installedServices.size() == 0) { 
+    		// Vérifier si le service a un port déjà utilisé ou s'il partage un identifiant avec un autre service
+	        if (!installedServices.contains(service)) {
+	            installedServices.add(service);
+	        } else {
+	        	throw new IllegalArgumentException("Un service similaire est déjà installé ici.");
+	        }
+        } else {
+        	throw new IllegalArgumentException("Un Container ne peut avoir qu'un service maximum installé.");
+        }
+    }
+    
 }
