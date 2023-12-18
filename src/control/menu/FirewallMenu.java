edb40bcd28ed98c9dfca53631dfa5a1d05ad6bff
Wizerr <127906404+Wizerr@users.noreply.github.com>
2023-12-18 14:13:57 +0100

    Update FirewallMenu.java

--- src/control/menu/FirewallMenu.java
+++ src/control/menu/FirewallMenu.java
@@ -7,9 +7,9 @@ public class FirewallMenu extends Menu<FirewallMenu.FirewallMenuAction> {
 		// Add all the menu items
 		this.addItem("1", "List all firewall rules for this server", FirewallMenuAction.LIST);
 		this.addItem("2", "Add a rule to this server's firewall", FirewallMenuAction.ADD);
-		this.addItem("4", "Delete a rule to this server's firewall", FirewallMenuAction.DELETE);
-		this.addItem("5", "Test if a service is available on this server", FirewallMenuAction.TEST);
-		this.addItem("6", "Test if a port is open on this server", FirewallMenuAction.EXIT);
+		this.addItem("3", "Delete a rule to this server's firewall", FirewallMenuAction.DELETE);
+		this.addItem("4", "Test if a port is open on this server", FirewallMenuAction.TEST);
+		this.addItem("5", "Exit firewall menu", FirewallMenuAction.EXIT);
 	}
 
 	/*
