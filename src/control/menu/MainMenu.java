d09940f74ce3cc18532f1bf34dc4d2886dab24a2
Wizerr <127906404+Wizerr@users.noreply.github.com>
2023-12-23 21:50:07 +0100

    Update MainMenu.java

--- src/control/menu/MainMenu.java
+++ src/control/menu/MainMenu.java
@@ -9,8 +9,8 @@ public class MainMenu extends Menu<MainMenu.MainMenuAction> {
 		this.addItem("2", "Select a server", MainMenuAction.INFO);
 		this.addItem("3", "Add a server", MainMenuAction.ADD);
 		this.addItem("4", "Delete a server", MainMenuAction.DELETE);
-		this.addItem("5", "Exit program", MainMenuAction.EXIT_PROGRAM);
-		this.addItem("9", "Object menu", MainMenuAction.EXAMPLE_OBJECT);
+		this.addItem("5", "Move a server", MainMenuAction.MOVE);
+		this.addItem("6", "Exit program", MainMenuAction.EXIT_PROGRAM);
 	}
 
 	/*
@@ -18,7 +18,7 @@ public class MainMenu extends Menu<MainMenu.MainMenuAction> {
 	 */
 	public enum MainMenuAction implements MenuAction {
 
-		LIST, INFO, ADD, DELETE, EXAMPLE_OBJECT, EXIT_PROGRAM
+		LIST, INFO, ADD, DELETE, MOVE, EXIT_PROGRAM
 
 	}
 
