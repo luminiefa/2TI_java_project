524a78a9e3519e85218712cf08ea5eb27b3f53c8
Wizerr <127906404+Wizerr@users.noreply.github.com>
2023-12-23 21:51:11 +0100

    Update BareMetal.java

--- src/model/BareMetal.java
+++ src/model/BareMetal.java
@@ -9,16 +9,31 @@ public class BareMetal extends Server {
     public BareMetal(int id) {
     	super(id);
         hostedServers = new HashSet<>();
+        this.type = "BMetal";
     }
 
+    @Override
     public void addHostedServer(Server server) {
         if (!(server instanceof BareMetal)) {	
         	//les serveurs BareMetal ne peuvent pas être hébergés
             hostedServers.add(server);
+            server.setHost(this);
         } else {
-            throw new IllegalArgumentException("Un serveur BareMetal ne peut pas être hébergé.");
+            throw new IllegalArgumentException("A BareMetal server cannot be hosted.");
         }
     }
+    
+    
+    @Override
+	public void deleteHostedServer(Server server) {
+		if (hostedServers.contains(server)) {
+			hostedServers.remove(server);
+			server.setHost(null);
+		} else {
+            throw new IllegalArgumentException("The server you are trying to remove is not hosted here.");
+        }
+	}
+	
     
     @Override
     public void setState(State state) {
