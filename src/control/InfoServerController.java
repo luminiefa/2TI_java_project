059c69a5712aceffd1f26b8d254b4b228d27b2e0
Wizerr <127906404+Wizerr@users.noreply.github.com>
2023-12-23 21:44:51 +0100

    Update InfoServerController.java

--- src/control/InfoServerController.java
+++ src/control/InfoServerController.java
@@ -1,6 +1,10 @@
 package control;
 
 import control.menu.InfoServerMenu;
+import data.DataSource;
+import model.Enumeration.State;
+import model.Server;
+import ui.output.Output;
 
 /**
  * Controller for the object menu
@@ -16,6 +20,9 @@ public class InfoServerController extends MenuController<InfoServerMenu.InfoServ
 
 		// Execute some action
 		switch (this.getMenuAction()) {
+		case SWITCH:
+			this.switchServerState();
+			break;
 		case SERVICE:
 			new ServiceController().start();
 			break;
@@ -26,5 +33,18 @@ public class InfoServerController extends MenuController<InfoServerMenu.InfoServ
 			this.exitRequest();
 			break;
 		}
+
 	}
+
+	public void switchServerState() {
+		Server server = DataSource.selectedServer;
+		if (server.getState() == State.DOWN) {
+			server.setState(State.UP);
+			Output.message("Server " + server.getId() + " status has been changed to UP.");
+		} else {
+			server.setState(State.DOWN);
+			Output.message("Server " + server.getId() + " status has been changed to DOWN.");
+		}
+	}
+
 }
